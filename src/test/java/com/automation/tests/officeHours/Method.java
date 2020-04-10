1383bc964336e85ec529774c18fcf9cdb48eb8a3
Gunel Khantamirova <gun040el@gmail.com>
2020-04-10 12:35:20 -0400

    Method

--- src/test/java/com/automation/tests/officeHours/Method.java
+++ src/test/java/com/automation/tests/officeHours/Method.java
@@ -3,5 +3,6 @@ package com.automation.tests.officeHours;
 public class Method {
     public static void main(String[] args) {
         System.out.println("Selenium");
+        System.out.println();
     }
 }
