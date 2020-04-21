c0ee7deba8e5d86977e3c6b48954ef7dde2fed41
Gunel Khantamirova <gun040el@gmail.com>
2020-04-21 10:48:24 -0400

    day 28_Write_exel_io

--- src/test/java/com/automation/tests/day25_excel_io/ReadDataFromExcel.java
+++ src/test/java/com/automation/tests/day25_excel_io/ReadDataFromExcel.java
@@ -11,7 +11,7 @@ import java.util.Map;
  * PLEASE DON'T WORRY ABOUT QUESTION MARK ON THE EXCEL FILE ICON
  */
 public class ReadDataFromExcel {
-/**
+
     @Test
     public void readExcelFileTest() throws Exception {
         //we need to get a file as an object
@@ -83,47 +83,6 @@ public class ReadDataFromExcel {
 
         System.out.println(excelUtil.getColumnsNames());
     }
-
-    */
-
-   @Test
-     public void readExcelFileTest() throws Exception{
-         //we need to get a file as an object
-         File file = new File("VytrackTestUsers.xlsx");
-      // object that represents excel file
-         Workbook workbook = WorkbookFactory.create(file);
-         Sheet workSheet = workbook.getSheet("QA1-short");
-
-         Row firstRow = workSheet.getRow(0);
-
-         Cell firstCell = firstRow.getCell(0);
-
-         String value = firstCell.getStringCellValue();
-
-         String secondCellValue = firstRow.getCell(1).getStringCellValue();
-
-         System.out.println(value);
-       System.out.println(secondCellValue);
-
-       int lastCell = firstRow.getLastCellNum();
-
-       System.out.println("#####################");
-       for (int i =0; i< lastCell; i++){
-           System.out.print(firstRow.getCell(i) +" | ");
-       }
-
-       //last row is 16th -->index is 15
-       //index of last row
-       int numberOfRows = workSheet.getLastRowNum();
-       //returns how many rows at all
-       int numberOfRows2 = workSheet.getPhysicalNumberOfRows();
-
-       System.out.println("\nIndex of last rows: " + numberOfRows);
-       System.out.println("\nNumbers of rows: " + numberOfRows2);
-
-     }
-
-
 
 }
 
---
+++ src/test/java/com/automation/tests/day28_write_into_excel/WriteIntoExcelFile.java
@@ -0,0 +1,30 @@
+package com.automation.tests.day28_write_into_excel;
+
+import org.apache.poi.ss.usermodel.*;
+import org.testng.annotations.Test;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+public class WriteIntoExcelFile {
+    @Test
+    public void writeIntoFileTest() throws IOException {
+        FileInputStream inputStream = new FileInputStream("VytrackTestUsers.xlsx");
+        Workbook workbook = WorkbookFactory.create(inputStream);
+        inputStream.close();
+        Sheet sheet = workbook.getSheet("QA3-short");
+        Row row = sheet.getRow(1);//2nd row
+        Cell cell = row.getCell(row.getLastCellNum() - 1);//last column
+        System.out.println("Before: "+cell.getStringCellValue());
+        cell.setCellValue("PASSED");//I am changing from n/a to passed
+        System.out.println("After: "+cell.getStringCellValue());
+
+        Row firstRow = sheet.getRow(0);
+
+        //I crete if I want to write something into the file
+        //don't forget to close excel file if you opened it
+        FileOutputStream outputStream = new FileOutputStream("VytrackTestUsers.xlsx");
+        workbook.write(outputStream);
+        workbook.close();
+    }
+}
